87b2469234355642a879e592687119eb2a379cc9
MateusMo <mat.marq.oliveira@gmail.com>
2023-09-07 18:46:09 -0300

    Feature: Adding the asp-for on add component

--- Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
+++ Bloggie.Web/Pages/Admin/Blogs/Add.cshtml.cs
@@ -5,8 +5,32 @@ namespace Bloggie.Web.Pages.Admin.Blogs
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public string Heading { get; set; }
+        [BindProperty]
+        public string PageTitle { get; set; }
+        [BindProperty]
+        public string Content { get; set; }
+        [BindProperty]
+        public string ShortDescription { get; set; }
+        [BindProperty]
+        public string ImageUrl { get; set; }
+        [BindProperty]
+        public string UrlHandle { get; set; }
+        [BindProperty]
+        public DateTime PublishedDate { get; set; }
+        [BindProperty]
+        public string Author { get; set; }
+        [BindProperty]
+        public bool IsVisible { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public void OnPost() 
+        {
+
         }
     }
 }
